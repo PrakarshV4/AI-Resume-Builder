1cd66f06d01a201ec0957689c7986a49552e454f
Prakarsh Verma <prakarsh1248@gmail.com>
2024-07-24 16:43:06 +0530

    Added Authentication Using Clerk

--- src/main.jsx
+++ src/main.jsx
@@ -6,20 +6,24 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import SignInPage from './auth/sign-in/index.jsx'
 import Home from './home/index.jsx'
 import Dashboard from './dashboard/index.jsx'
+import { ClerkProvider } from '@clerk/clerk-react'
 
-const router = createBrowserRouter([
-  {
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY //for clerk
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
+
+const router = createBrowserRouter([{
     element:<App/>,
-    children:[
-    {
-      path: '/',
-      element:<Home/>
-    },
-    {
+    children:[{
       path: '/dashboard',
       element:<Dashboard/>
     },
   ]
+  },
+  {
+    path: '/',
+    element:<Home/>
   },
   {
     path:'/auth/sign-in',
@@ -28,6 +32,8 @@ const router = createBrowserRouter([
 ])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      <RouterProvider router={router}/>
+    </ClerkProvider>
   </React.StrictMode>,
 )
